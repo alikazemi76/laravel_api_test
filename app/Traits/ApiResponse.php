2a962d6a3b354b9db93e54246d6ea1605b330bc7
Ali Kazemipour <ali.kazemipour@yahoo.com>
2021-11-25 17:21:39 +0330

    edit and update

--- app/Traits/ApiResponse.php
+++ app/Traits/ApiResponse.php
@@ -2,14 +2,23 @@
 namespace  App\Traits;
 
 
+
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+
 trait ApiResponse
 {
-    public function responseSuccess($data, $messages = [], $code = 200)
+    public function responseSuccess($data, $messages = [], $code = 200 ,$meta=null)
     {
+        if( $data instanceof AnonymousResourceCollection ){
+            if(isset($data->response()->getDate()->meta)){
+                $meta=$data->response()->getDate()->meta;
+            }
+        }
         return response()->json([
             'data' => $data,
             'success' => true,
-            'messages' => $messages
+            'messages' => $messages,
+            'meta'=>$meta
         ], $code);
     }
 
